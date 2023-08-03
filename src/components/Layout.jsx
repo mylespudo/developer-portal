88743ebcf5a6f7def8b7cd299df3d8ca2cc3d3e8
arkhurst <fiiarkhurst@gmail.com>
2023-08-03 14:12:31 +0000

    added supported region content

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -22,9 +22,9 @@ export const navigation = [
       { title: 'Supported countries', href: '/docs/supported-countries' },
       {
         title: 'Supported regions',
-        href: '/docs/predicting-user-behavior',
+        href: '/docs/supported-regions',
       },
-      { title: 'Supported cities', href: '/docs/basics-of-time-travel' },
+      { title: 'Supported cities', href: '/docs/supported-cities' },
       {
         title: 'Billing',
         href: '/docs/introduction-to-string-theory',
